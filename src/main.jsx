c5bfd587547d925aad7a32957ccc29e8a40b3a30
WIBU1234 <210711186@students.uajy.ac.id>
2025-02-17 20:41:44 +0700

    Commit Route Fix With ProtectedRoutes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import AppRouter from './route/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppRouter />
   </StrictMode>,
 )
